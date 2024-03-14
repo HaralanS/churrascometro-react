4b5d265433fbe19ce4a7961b80f2fb245edbdacb
HaralanS <haralansantana@gmail.com>
2024-03-14 10:36:58 -0300

    update environment variable

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -14,8 +14,8 @@ export const getChurras = async <T>(): Promise<AxiosResponse<T, any>> => {
   return axios.get(BASE_URL)
 }
 
-export const excluir = (id: string) => axios.delete(`${BASE_URL}${id}`);
+export const excluir = (id: string) => axios.delete(`${BASE_URL}/${id}`);
 
 export const edit = (id: any, data:any, carne: number, paoDeAlho:number, refri:number, cerveja:number, carvao:number, pessoas:number) => {
-  return axios.put(`${BASE_URL}${id}`, ({id: id,data:data.data, homens: Number(data.homens), mulheres: Number(data.mulheres), criancas: Number(data.criancas), carne, paoDeAlho, refri, cerveja, carvao, pessoas}))
+  return axios.put(`${BASE_URL}/${id}`, ({id: id,data:data.data, homens: Number(data.homens), mulheres: Number(data.mulheres), criancas: Number(data.criancas), carne, paoDeAlho, refri, cerveja, carvao, pessoas}))
 }
